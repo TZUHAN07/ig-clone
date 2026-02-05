1e258bce2e952b4864609b3895f069fa48a695e3
Tzu Han Chao <joannechao1007@gmail.com>
2026-02-06 00:28:43 +0800

    refactor: 改調整 postModel 檔名並移除 console.log

---
+++ models/postModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: [true, "文字內容是必填的"]
+    },
+    image: {
+      type: String,
+      required: [true, "圖片是必填的"]
+    },
+  },
+  {
+    versionKey: false,
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Post", postSchema);
