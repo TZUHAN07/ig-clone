d9224222672df05ec24c758bb93ddb7ad813f120
Tzu Han Chao <joannechao1007@gmail.com>
2026-03-16 21:53:08 +0800

    feat: 新增追蹤功能

---
+++ controllers/followController.js
@@ -0,0 +1,146 @@
+const User = require("../models/userModel");
+
+const followUser = async (req, res) => {
+  const targetId = req.params.id;
+  const myId = req.user._id;
+  console.log(targetId, myId);
+
+  try {
+    if (targetId === myId.toString()) {
+      return res.status(400).json({
+        success: false,
+        message: "不能追蹤自己",
+      });
+    }
+
+    const targetUser = await User.findById(targetId);
+
+    if (!targetUser) {
+      return res.status(404).json({
+        success: false,
+        message: "找不到使用者",
+      });
+    }
+    if (targetUser.followers.some(id => id.equals(myId))) {
+      return res.status(400).json({
+        success: false,
+        message: "已追蹤過此使用者",
+      });
+    }
+    await User.findByIdAndUpdate(targetId, { $push: { followers: myId } });
+    await User.findByIdAndUpdate(myId, { $push: { following: targetId } });
+
+    res.status(200).json({
+      success: true,
+      message: "追蹤成功",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+const unfollowUser = async (req, res) => {
+  const targetId = req.params.id;
+  const myId = req.user._id;
+
+  try{
+    if (targetId === myId.toString()) {
+      return res.status(400).json({
+        success: false,
+        message: "不能取消追蹤自己",
+      });
+    }
+
+    const targetUser = await User.findById(targetId);
+
+    if(!targetUser){
+      return res.status(404).json({
+        success: false,
+        message: "找不到使用者",
+      });
+    }
+
+    if(!targetUser.followers.some(id => id.equals(myId))){
+      return res.status(400).json({
+        success: false,
+        message: "未追蹤此使用者",
+      });
+    }
+
+    await User.findByIdAndUpdate(targetId, { $pull: { followers: myId } });
+    await User.findByIdAndUpdate(myId, { $pull: { following: targetId } });
+    res.status(200).json({
+        success: true,
+        message: "已取消追蹤"
+    })
+  }catch(err){
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+const getFollowers =async (req, res) => {
+    try{
+        const  user = await User.findById(req.params.id)
+        .select("followers")
+        .populate("followers", "username avatar");
+        
+
+        if (!user){
+            return res.status(404).json({
+                success: false,
+                message: "找不到使用者"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user.followers
+        })
+    }catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+const getFollowing =async (req, res) => {
+    try{
+        const  user = await User.findById(req.params.id)
+        .select("following")
+        .populate("following", "username avatar");
+        
+
+        if (!user){
+            return res.status(404).json({
+                success: false,
+                message: "找不到使用者"
+            })
+        }   
+
+        res.status(200).json({
+            success: true,
+            data: user.following
+        })  
+                                
+    }catch(err){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+
+module.exports = {
+  followUser,
+  unfollowUser,
+  getFollowers,
+  getFollowing
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 
 const {
   getAllUsers,
@@ -10,16 +11,22 @@ const {
   getMe,
 } = require("../controllers/userController");
 
-const upload = require("../middleware/uploadMiddleware");
+const {
+  followUser,
+  unfollowUser,
+  getFollowers,
+  getFollowing,
+} = require("../controllers/followController");
 
 router.get("/", getAllUsers);
-
 router.get("/me", authMiddleware, getMe);
-
 router.get("/:id", getUser);
-
-router.put("/:id", authMiddleware,  upload.single("avatar"), updateUser);
-
+router.put("/:id", authMiddleware, upload.single("avatar"), updateUser);
 router.delete("/:id", authMiddleware, deleteUser);
+
+router.post("/:id/follow", authMiddleware, followUser);
+router.delete("/:id/unfollow", authMiddleware, unfollowUser);
+router.get("/:id/followers", getFollowers);
+router.get("/:id/following", getFollowing);
 
 module.exports = router;
