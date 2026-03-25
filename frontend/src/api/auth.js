c79045da9500969be597759a5251291e2ce81243
Tzu Han Chao <joannechao1007@gmail.com>
2026-03-25 23:59:39 +0800

    feat:新增登入註冊頁面和串接後端api

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,65 @@
+const { API_BASE_URL } = config;
+const message = document.getElementById("error-message");
+
+const loginApi = async function login(email, password) {
+  try {
+    const res = await fetch(`${API_BASE_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+
+    if (data.success) {
+      localStorage.setItem("token", data.token);
+
+      message.innerText = "登入成功，歡迎回來！";
+      message.style.color = "#28a745";
+
+      setTimeout(() => {
+        window.location.href = "index.html"; 
+      }, 1000);
+    } else {
+      alert(data.message);
+    }
+  } catch (err) {
+    message.innerText = "註冊失敗";
+  }
+};
+
+const registerApi = async function register(username, email, password) {
+  try {
+    const res = await fetch(`${API_BASE_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+
+    if (data.success) {
+      message.innerText = "註冊成功！正在為您跳轉...";
+      message.style.color = "#28a745";
+
+      setTimeout(() => {
+        window.location.href = "login.html";
+      }, 1500);
+    } else {
+      message.innerText = "伺服器連線失敗";
+    }
+  } catch (err) {
+    console.log("註冊錯誤", err);
+  }
+};
---
+++ frontend/src/page/validation.js
@@ -0,0 +1,96 @@
+const form = document.getElementById("form");
+const usernameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const repeatPasswordInput = document.getElementById("repeat-password");
+const errorMessage = document.getElementById("error-message");
+const submitButton = document.getElementById("submit-button");
+
+console.log("validation loaded");
+form.addEventListener("submit", async(e) => {
+   console.log("submit triggered");
+   e.preventDefault() 
+  let errors = [];
+
+  if (usernameInput) {
+    errors = getSignupFormErrors(
+      usernameInput.value,
+      emailInput.value,
+      passwordInput.value,
+      repeatPasswordInput.value,
+    );
+
+  
+    if(errors.length === 0){
+      await registerApi(usernameInput.value, emailInput.value, passwordInput.value);
+    }
+  } else {
+    errors = getLoginFormErrors(emailInput.value, passwordInput.value);
+    if (errors.length === 0) {
+      await loginApi(emailInput.value, passwordInput.value);
+    }
+  }
+
+  if (errors.length > 0) {
+    errorMessage.innerText = errors.join("|");
+  }
+});
+
+function getSignupFormErrors(username, email, password, repeatPassword) {
+  let errors = [];
+
+  if (username.trim() === "") {
+    errors.push("請輸入使用者名稱");
+    usernameInput.parentElement.classList.add("incorrect");
+  }
+
+  if (email.trim() === "") {
+    errors.push("請輸入電子郵件");
+    emailInput.parentElement.classList.add("incorrect");
+  }
+
+  if (password.trim() === "") {
+    errors.push("請輸入密碼");
+    passwordInput.parentElement.classList.add("incorrect");
+  }
+
+  if (password.length <8){
+    errors.push("密碼長度至少為8");
+    passwordInput.parentElement.classList.add("incorrect");
+  }
+
+  if (password !== repeatPassword) {
+    errors.push("密碼不一致");
+    passwordInput.parentElement.classList.add("incorrect");
+    repeatPasswordInput.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+}
+
+const getLoginFormErrors = (email, password) => {
+  let errors = [];
+
+  if (email.trim() === "") {
+    errors.push("請輸入電子郵件");
+    emailInput.parentElement.classList.add("incorrect");
+  }
+
+  if (password.trim() === "") {
+    errors.push("請輸入密碼");
+    passwordInput.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+};
+
+const allInputs = Array.from(document.querySelectorAll("input"))
+allInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.parentElement.classList.contains("incorrect")) {
+      input.parentElement.classList.remove("incorrect");
+      errorMessage.innerText = "";
+    }
+  });
+});
+
