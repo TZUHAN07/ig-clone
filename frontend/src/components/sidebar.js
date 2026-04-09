c9078df97e23eb272dc72f2ea70cf13395433528
Tzu Han Chao <joannechao1007@gmail.com>
2026-04-09 21:52:57 +0800

    feat: 新增logout按鈕樣式

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -90,6 +90,22 @@ function loadSidebar() {
         </li>
         <li class="profile"></li>
       </ul>
+
+      <div class="sidebar-bottom">
+          <button class="logout-btn nav-action">
+             <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                height="24px" 
+                viewBox="0 -960 960 960" 
+                width="24px" 
+                fill="#e8eaed"
+                >
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+                />
+                </svg>
+             <span>Logout</span>
+          </button>
+      </div>
     `;
 
   sidebarContainer.appendChild(sidebar);
