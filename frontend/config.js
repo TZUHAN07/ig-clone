6a2b9de02f7978621598421cbda8f421578cf702
Tzu Han Chao <joannechao1007@gmail.com>
2026-04-29 21:49:59 +0800

    feat: 新增 Docker 與 Nginx 設定

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,6 @@
 const config = {
-  API_BASE_URL: "http://localhost:3000",
+  API_BASE_URL:
+    window.location.hostname === "localhost" && window.location.port !== "80"
+      ? "http://localhost:3000"
+      : "/api",
 };
