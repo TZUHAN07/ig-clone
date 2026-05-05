c56be8bbd7288cd79f805de741e29f11a2a3e7b2
Tzu Han Chao <joannechao1007@gmail.com>
2026-05-06 02:01:00 +0800

    feat: 新增 rate limiting 防止暴力破解

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,45 @@ const http = require("http");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const {initSocket} = require("./config/socket");
+const { initSocket } = require("./config/socket");
+const rateLimit = require("express-rate-limit");
 
 const postRoute = require("./routes/postRoutes");
 const authRoute = require("./routes/authRoutes");
-const userRoute = require("./routes/userRoutes")
+const userRoute = require("./routes/userRoutes");
 
 const app = express();
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    success: false,
+    message: "嘗試次數過多，請 15 分鐘後再試",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: "請求過於頻繁，請稍後再試",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/", authLimiter);
+app.use("/posts", apiLimiter);
+app.use("/users", apiLimiter);
+
 app.use("/posts", postRoute);
 app.use("/", authRoute);
 app.use("/users", userRoute);
