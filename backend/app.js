166102269f3624c73364d6588db29934a0ac3c42
Tzu Han Chao <joannechao1007@gmail.com>
2026-06-18 16:22:43 +0800

    fix: 提升上傳 size limit 至 20MB

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ app.set("trust proxy", 1);
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.use("/posts", apiLimiter, postRoute);
 app.use("/", authRoute);
